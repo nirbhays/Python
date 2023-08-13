af7cd5617e9ecf9923be5a9500ae6d7bb80bfca9
nirbhays <sin.nirbhaysingh@gmail.com>
2023-08-13 20:27:57 +0200

    BMI

---
+++ Odd_or_Even.py
@@ -0,0 +1,7 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+if number%2==1:
+    print(f"It is a odd number{number}")
+else: 
+    print(f"This is an even {number}")

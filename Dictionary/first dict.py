029e1b4d34b99c543d927452ba5cba460d5c5899
bsh-nirbhay <163397429+bsh-nirbhay@users.noreply.github.com>
2024-03-25 00:11:43 +0100

    grading program

---
+++ Dictionary/first dict.py
@@ -0,0 +1,17 @@
+programming_dictionary ={}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again."
+}
+programming_dictionary={
+    "nirbhay": "is a singh",
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again."
+    }
+#print(programming_dictionary["nirbhay"])
+#print(programming_dictionary["Function"])
+programming_dictionary["zoop"]="soop"
+#print(programming_dictionary)
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
